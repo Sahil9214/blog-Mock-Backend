0a2a134f069060a9a124eaff7e7c2fd6440dc73d
Utkarsh <utkarshsinghal369@gmail.com>
2023-08-24 18:11:51 +0530

    Mock 6

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const { connection } = require("./db");
 const app = express();
-const {authenticate}=require('./Middleware/auth.middleware')
+const { authenticate } = require("./Middleware/auth.middleware");
 const { userRouter } = require("./Routes/User.Routes");
 const { blogRouter } = require("./Routes/Blog.Routes");
 
 require("dotenv").config();
 app.use(express.json());
 app.use("/user", userRouter);
-app.use(auth)
+app.use(auth);
 app.use("/blog", blogRouter);
 
 app.listen(process.env.PORT, async () => {
